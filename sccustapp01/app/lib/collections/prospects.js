b1c051b0347360aadc0105b5eab411ee5ba1ee74
German Arduino <garduino@gmail.com>
2015-11-21 23:49:23 -0300

    Initial Commit
    sccustapp01 is developed following
    https://scotch.io/tutorials/how-to-speed-up-meteor-development-with-scaf
    folding-and-automatic-form-generation

---
+++ sccustapp01/app/lib/collections/prospects.js
@@ -0,0 +1,18 @@
+Prospects = new Mongo.Collection('prospects');
+
+
+if (Meteor.isServer) {
+  Prospects.allow({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
